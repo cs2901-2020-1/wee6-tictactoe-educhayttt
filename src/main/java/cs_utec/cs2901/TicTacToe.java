e0440f945fe18d9d4d9890af14008c514825229f
luisejv <ljauregui@pucp.pe>
2020-05-14 12:26:34 -0500

    read added

--- src/main/java/cs_utec/cs2901/TicTacToe.java
+++ src/main/java/cs_utec/cs2901/TicTacToe.java
@@ -23,4 +23,24 @@ public class TicTacToe {
         if (counter == 0)
             System.out.println("No winners");
     }
+
+     private void read() {
+        System.out.print("Ingrese Coordenadas: ");
+        String[] line = scanner.nextLine().split(",");
+        if (line[0]>2 || line[1]>2 || line[0]<0 || line[1]<0){
+            if (matrix[Integer.parseInt(line[0])][Integer.parseInt(line[1])].equals(" "))
+            {
+                matrix[Integer.parseInt(line[0])][Integer.parseInt(line[1])] = symbol;
+                --counter;
+            }
+            else
+            {
+                System.out.println("Posición ya ocupada. Intente nuevamente.");
+                read();
+            }
+        } else {
+            System.out.println("Indexes incorrectos. Intente nuevamente.");
+            read();
+        }
+    }
 }
